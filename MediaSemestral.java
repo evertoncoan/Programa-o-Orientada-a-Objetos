64979f27e6d3bcb018f72870a6440211925ca0f8
evertoncoan <everton_coan@hotmail.com>
2016-04-12 21:52:33 -0300

    Criei o programa que calcula a média semestral.

---
+++ MediaSemestral.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Everton
+ *
+ */
+public class MediaSemestral {
+
+	/**
+	 * 
+	 */
+	public MediaSemestral() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
