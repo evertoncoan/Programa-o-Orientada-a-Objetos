69e3248ed22435efd71eeffb3b54fa72541b6ccd
evertoncoan <everton_coan@hotmail.com>
2016-09-12 15:55:43 -0300

    Add files via upload

---
+++ POO/impares.java
@@ -0,0 +1,16 @@
+package impares;
+
+public class impares 
+{
+	public static void main(String[] args)
+	{
+		int impar = -1;
+		
+		for (int k = 0; k < 25; k = k + 1)
+		{
+			impar = impar + 2;
+			System.out.printf(impar + "\n");
+		}
+	}
+
+}
