92509c98c068c01e47dd4de86311b5225577c7cd
evertoncoan <everton_coan@hotmail.com>
2016-09-28 17:26:09 -0300

    Add files via upload

---
+++ POO/Lista%20Exerc%C3%ADos%20Arrays/Exercicio6.java
@@ -0,0 +1,29 @@
+package exercicio6;
+
+import java.util.Scanner;
+
+public class Exercicio6
+{
+	public static void main(String[] args)
+	{
+		Scanner entrada = new Scanner(System.in);
+		
+		double elemento;
+		double[] arrayElementos = new double[40];
+		
+		for (int i = 0; i < 40; i++)
+		{
+			System.out.println("Insira um número: ");
+			elemento = entrada.nextDouble();
+			if ( elemento < 0)
+				elemento = 0;
+			arrayElementos[i] = elemento;
+		}
+		
+		for (int i = 0; i < 40; i++)
+		{
+			System.out.println(arrayElementos[i]);
+		}
+		entrada.close();
+	}
+}
---
+++ POO/Lista%20Exerc%C3%ADos%20Arrays/Exercicio7.java
@@ -0,0 +1,43 @@
+package exercicio7;
+
+import java.util.Scanner;
+
+public class Exercicio7
+{
+	public static void main(String[] args)
+	{
+		Scanner entrada = new Scanner(System.in);
+		
+		double elemento1, elemento2;
+		double[] array1 = new double[20];
+		double[] array2 = new double[20];
+		double[] array3 = new double[40];
+		int k = 0, n = 1;
+		
+		for (int i = 0; i < 20; i++)
+		{
+			System.out.println("Insira um número "
+					+ "para o primeiro vetor: ");
+			elemento1 = entrada.nextDouble();
+			array1[i] = elemento1;
+			array3[k] = elemento1;
+			k = k + 2;
+		}
+		
+		for (int i = 0; i < 20; i++)
+		{
+			System.out.println("Insira um número "
+					+ "para o segundo vetor: ");
+			elemento2 = entrada.nextDouble();
+			array2[i] = elemento2;
+			array3[n] = elemento2;
+			n = n + 2;
+		}
+		
+		for (int i = 0; i < 40; i++)
+		{
+			System.out.println(array3[i]);
+		}
+		entrada.close();
+	}
+}
