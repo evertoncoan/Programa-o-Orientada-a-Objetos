b47d1087e46abd903d1cad1d98627d74a7af9474
evertoncoan <everton_coan@hotmail.com>
2016-09-28 16:42:38 -0300

    Update Exercício 5.java

--- POO/Lista-de-Exercicios-Array/Exercício 5.java
+++ POO/Lista-de-Exercicios-Array/Exercício 5.java
@@ -1,16 +1,16 @@
-Lista de Exercícios Vetores.
-Exercício 5.
+Lista de ExercÃ­cios Vetores.
+ExercÃ­cio 5.
 
 Inicio
 	real elemento, arrayElementos[40];
 	inteiro pares;
 	
-	para (i = 0, i < 40, i++0){
+	para (inteiro i = 0, i < 40, i++0){
 		leia elemeto;
 		arrayElementos[i] = elemento;
 	}
 	
-	para (i = 0, i < 40, i++){
+	para (inteiro i = 0, i < 40, i++){
 		se (arrayElementos[i] mod 2 == 0){
 			escreva arrayElementos[i];
 			pares += 1;
