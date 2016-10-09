6acbf2a53ecb137566eaa51b9c5b8aa365f25e69
evertoncoan <everton_coan@hotmail.com>
2016-10-09 11:35:08 -0300

    Rename POO/Lista%20Exerc%C3%ADos%20Arrays/Exercicio5.java to Lista-Exercicios-Arrays/Exercicio5.java

--- Lista-Exercicios-Arrays/Exercicio5.java
+++ Lista-Exercicios-Arrays/Exercicio5.java
@@ -14,7 +14,7 @@ public class Exercicio5
 		
 		for (int i = 0; i < 40; i++)
 		{
-			System.out.println("Insira um número: ");
+			System.out.println("Insira um nÃºmero: ");
 			elemento = entrada.nextDouble();
 			arrayElementos[i] = elemento;
 		}
