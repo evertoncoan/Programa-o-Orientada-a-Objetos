8ca4c6483796acdfc5ac6fa388b969addd43291c
evertoncoan <everton_coan@hotmail.com>
2016-10-10 13:03:50 -0300

    Terminei o exercício 2.

--- POO/Lista-de-Exercicios-Matrizes/Exercicio-2.java
+++ POO/Lista-de-Exercicios-Matrizes/Exercicio-2.java
@@ -2,3 +2,42 @@ Lista de Exercícios Matrizes.
 Exercício 2.
 
 Inicio
+	inteiro n = 0, m = 0;
+	boolean bool = true;
+	
+	leia n;
+	leia m;
+	
+	real matrizElementos[n][m];
+	
+	para (inteiro i = 0; i < n; i++){
+		para (inteiro j = 0; j < m; j++){
+			leia matrizElementos[i][j];
+		}
+	}
+	
+	para (inteiro i = 0; i < n; i++){
+		para (inteiro j = 0; j < m; j++){
+			inteiro k = 0;
+			se (matrizElementos[i][j] == 0)
+				k = k + 1;
+		}
+		se (k != n - 1)
+			bool = false;
+	}
+	
+	para (inteiro i = 0; i < m; i++){
+		para (inteiro j = 0; j < n; j++){
+			inteiro k = 0;
+			se (matrizElementos[i][j] == 0)
+				k = k + 1;
+		}
+		se (k != m - 1)
+			bool = false;
+	}
+	
+	se (bool = true)
+		escreva "A matriz é uma permutação."
+	senão
+		escreva "A matriz não é uma permutação."
+Fim
---
+++ POO/Lista-de-Exercicios-Matrizes/Exercicio-3.java
@@ -0,0 +1,5 @@
+Lista de Exercícios Matrizes.
+Exercício 3.
+
+Inicio
+	
