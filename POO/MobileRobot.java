1116f9154ac758b4b30f216a1772397d6b380e87
evertoncoan <everton_coan@hotmail.com>
2016-11-09 17:14:24 -0200

    Add files via upload

---
+++ POO/MobileRobot.java
@@ -0,0 +1,57 @@
+
+public class MobileRobot extends Robot {
+	private int    power;
+	private byte   status;
+	private double speed;
+	
+	public MobileRobot(){
+		super();
+	}
+	
+	public MobileRobot(String name){
+		super(name);
+	}
+	
+	public MobileRobot( String name, int temperature , int power,
+					    byte status, double speed )
+	{
+		super(name, temperature);
+		this.power = power;
+		this.status = status;
+		this.speed = speed;
+	}
+	
+	public int getPower(){
+		return power;
+	}
+	
+	public void setPower(int power){
+		this.power = power;
+	}
+	
+	public byte getStatus(){
+		return status;
+	}
+	
+	public void setStatus(byte status){
+		this.status = status;
+	}
+	
+	public double getSpeed(){
+		return speed;
+	}
+	
+	public void setSpeed(double speed){
+		this.speed = speed;
+	}
+	
+	public String toString(){
+		return "Nome: "          + this.getName() +
+			   "\nTemperatura: " + this.getTemperature() +
+			   "\nPower: "       + this.getPower() +
+			   "\nStatus: "      + this.getStatus() +
+			   "\nSpeed "        + this.getSpeed();
+	}
+	
+	
+}
---
+++ POO/Robot.java
@@ -0,0 +1,39 @@
+
+public class Robot {
+	private String name;
+	private double temperature;
+	
+	public Robot(){
+		super();
+	}
+	
+	public Robot(String name){
+		this.name = name;
+	}
+	
+	public Robot(String name, int temperature){
+		this.name        = name;
+		this.temperature = temperature;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public double getTemperature(){
+		return temperature;
+	}
+
+	public void setTemperature(double temperature){
+		this.temperature = temperature;
+	}
+	
+	public String toString(){
+		return "Nome: "          + this.getName() +
+			   "\nTemperatura: " + this.getTemperature();
+	}
+}
