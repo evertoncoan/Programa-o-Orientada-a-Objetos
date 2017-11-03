50af1c87c2fd4325b4f15e1720dae8281243a905
Everton Coan <Everton Coan>
2017-11-02 23:11:43 -0200

    .

--- Programacao Concorrente/Trabalho 2/Cliente.java
+++ Programacao Concorrente/Trabalho 2/Cliente.java
@@ -1,4 +1,9 @@
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
@@ -7,26 +12,31 @@ import java.util.List;
 
 public class Cliente
 {
-    public static void main(String[] args)
+    static boolean parar;
+
+    public static void main(String[] args) throws UnknownHostException, IOException
     {
         Socket socket = new Socket(InetAddress.getByName("127.0.0.1"), 5000);
-        
+
         ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
         out.flush();
         ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
-        
+
         Thread readThred = startReadThread(input);
-        
-        
-        
+
+        while (!parar)
+        {
+
+        }
+
         try { input.close(); } catch (IOException e) { e.printStackTrace(); }
         try { out.close(); } catch (IOException e) { e.printStackTrace(); }
         try { socket.close(); } catch (IOException e) { e.printStackTrace(); }
         try { readThred.join(); } catch (InterruptedException e)
         { e.printStackTrace(); }
     }
-    
-    private Thread startReadThread(ObjectInputStream input)
+
+    private static Thread startReadThread(ObjectInputStream input)
     {
         Thread readThread = new Thread()
         {
@@ -36,13 +46,15 @@ public class Cliente
                 {
                     while (true)
                     {
+                        System.out.println("oi 2");//----------------------------------------------------
                         String hash = (String) input.readObject();
+                        System.out.println("oi 5");//----------------------------------------------------
                         int intervaloA = (int) input.readObject();
                         int intervaloB = (int) input.readObject();
-                        
-                        System.out.println("Intervalo recebido do servidor de " +
-                        + intervaloA + "até" + intervaloB);
-                        
+
+                        System.out.println("Intervalo recebido do servidor de "
+                        + intervaloA + " até " + intervaloB);
+
                         codigo(intervaloA, intervaloB, hash);
                     }
                 } catch (Exception e)
@@ -50,32 +62,35 @@ public class Cliente
                     e.printStackTrace();
                 }
             }
-        }
+        };
+
+        readThread.start();
+        return readThread;
     }
-    
-    public void codigo(int intervaloA, int intervaloB, String hash)
+
+    public static void codigo(int intervaloA, int intervaloB, String hash) throws NoSuchAlgorithmException
     {
         long tempo = System.currentTimeMillis();
-        
+
         for (int i = intervaloA; i <= intervaloB; i++)
         {
             String numero = String.format("%07d", i);
-            
+
             String md5 = md5(numero);
-            
+
             if (md5.equals(hash))
             {
-                System.out.println("O código " + codigo +
+                System.out.println("O código " + hash
                 + " é produzido pelo número " + numero);
-                
+
                 tempo = System.currentTimeMillis() - tempo;
-                
-                System.out.println("O programa levou " + tempo +
+
+                System.out.println("O programa levou " + tempo
                 + "ms para encontrar esse número.");
             }
         }
     }
-    
+
     public static String md5(String entrada) throws NoSuchAlgorithmException
     {
         MessageDigest sha1 = MessageDigest.getInstance("MD5");
--- Programacao Concorrente/Trabalho 2/Servidor.java
+++ Programacao Concorrente/Trabalho 2/Servidor.java
@@ -1,32 +1,70 @@
-
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Servidor
 {
-    public static void main(String[] args)
+	private static List<String> codigos;
+    private static int max;
+    private static int intervaloA;
+    private static int intervaloB;
+
+    public static void main(String[] args) throws IOException
     {
+    	codigos = Files.readAllLines(Paths.get("hashes.txt"));
+        max = 9999999;
+        intervaloA = 0;
+        intervaloB = 199999;
+
         ServerSocket server = new ServerSocket(5000, 10);
         while (true)
         {
             Socket s = server.accept();
             System.out.println("Nova conexão realizada");
-            
+
             serveClient(s);
         }
     }
-    
-    private void serveClient(Socket socket) throws IOException
+
+    private static void serveClient(Socket socket) throws IOException
     {
         ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
         out.flush();
         ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
-        
+        System.out.println("oi 1");//--------------------------------------------------------------
+
         Thread socketThread = new Thread()
         {
             public void run()
             {
-                
+            	try
+            	{
+                System.out.println(codigos.get(0));//--------------------------------------------
+                out.writeObject(codigos.get(0));
+                out.flush();
+                System.out.println("oi 3");//----------------------------------------------------
+                out.writeObject(intervaloA);
+                System.out.println("oi 4");//----------------------------------------------------
+                out.flush();
+                out.writeObject(intervaloB);
+                out.flush();
+            	} catch (IOException e) {
+            		e.printStackTrace();
+            	}
+                try { input.close(); } catch (IOException e) {
+                    e.printStackTrace(); }
+                try { out.close(); } catch (IOException e) {
+                    e.printStackTrace(); }
+                try { socket.close(); } catch (IOException e) {
+                    e.printStackTrace(); }
             }
-        }
+        };
+        socketThread.start();
     }
 }
 
